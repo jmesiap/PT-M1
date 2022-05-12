ab2fbd981b19174ced7573b4627b4531ce9cd433
José Ivan Mesia Portocarrero <jmesia.portocarrero@gmail.com>
2022-05-12 14:36:15 -0500

    12/05/2022 - 2:31pm

--- 02-JavaScriptAvanzado-II/homework/homework.js
+++ 02-JavaScriptAvanzado-II/homework/homework.js
@@ -30,7 +30,22 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  var cache = new Object;
+  return function (arg){
+      if (cache.hasOwnProperty(arg)){
+          console.log('almacenado')
+          return cache[arg]; 
+      }
+          return cache[arg] =  cb(arg)
+      }    
 }
+function callB (arg){
+  var x= arg*arg
+  return x
+}
+
+const nuevafun = cacheFunction(callB)
+
 
 // Bind
 
@@ -50,8 +65,8 @@ function getNombre(){
  // Escribir código, sin modificar lo que ya se encuentra escrito arriba, para poder llamar al método getNombre para obtener primero el nombre del instructor y luego para obtener el nombre del alumno.
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que bindear el this!
-let getNombreInstructor = undefined;
-let getNombreAlumno = undefined;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 
 /*Guardar en las siguientes tres variables una función que devuelva una cadena utilizando la función "crearCadena"
@@ -70,12 +85,15 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena){
 // Modificar los undefined por el código correspondiente en cada caso
 // Pista, tenes que usar bind para "bindear" algunos parámetros de la función crearCadena.
 
-let textoAsteriscos = undefined;
+let textoAsteriscos = crearCadena.bind(this, '*', '*');
 
-let textoGuiones = undefined;
+let textoGuiones = crearCadena.bind(this, '-', '-');
 
-let textoUnderscore = undefined;
+let textoUnderscore = crearCadena.bind(this, '_', '_');
 
+textoAsteriscos(' Hola ')
+textoGuiones(' Hola ')
+textoUnderscore(' Hola ')
 
 
 // No modifiquen nada debajo de esta linea
