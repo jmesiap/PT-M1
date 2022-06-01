aa76d15c4182439149471517d49229bd34232f8c
José Ivan Mesia Portocarrero <jmesia.portocarrero@gmail.com>
2022-06-01 11:08:19 -0500

    01/06/2022 - 11:08am

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -6,6 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let factorprimo = [];
+  let div = 2;
+  while (num !=1){
+    if (num % div === 0){
+      factorprimo.push (div);
+      num = num / div
+    }else {
+      div++;
+    }
+  }
+  factorprimo.unshift(1);
+  return factorprimo
 
 }
 
@@ -15,6 +27,19 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  let swap = true;
+  while (swap){
+    swap = false;
+    for (let i = 0; i < array.length-1; i++ ){
+      if (array[i] > array[i+1]){
+          let aux = array[i+1];
+          array[i+1] = array[i];
+          array[i] = aux;
+          swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,8 +48,21 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+                             
+  for (let i = 1; i < array.length; i++){
+       let aux = array[i];  
+       let j = i-1 
+      while(j >= 0 && array[j] > aux){
+        array[j+1] = array[j];
+        array[j] = aux;
+        j--;
+      }
+   }
+  return array
+  
+  }
 
-}
+
 
 
 function selectionSort(array) {
@@ -33,6 +71,19 @@ function selectionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  
+  for(let i = 0; i < array.length; i++){
+    let min = i;
+   for(let j = i+1; j<array.length; j++){
+      if(array[j] <= array[min]){
+          min = j;
+      }
+    }
+    let aux = array[min]
+    array[min] = array[i]
+    array[i] = aux
+  }
+  return array
 }
 
 
